53ac930a3c86f0b4b68e7b438bd06f12e8e00094
mateotop <mathewtwi@ya.ru>
2022-08-07 19:01:49 +0300

    Fixes: Directories reorgonization and colour in buttons:hover

--- coffee_site/JS_tasks/2/js2.js
+++
@@ -1,2 +0,0 @@
-let result = prompt("Введите год вашего рождения:")
-alert(result)
--- coffee_site/JS_tasks/3/js3.js
+++
@@ -1,8 +0,0 @@
-btnElement = document.querySelector(".btn");
-resultElement = document.querySelector(".result");
-inputNameElement = document.querySelector(".name");
-inputSurnameElement = document.querySelector(".surname");
-
-btnElement.addEventListener("click", function(){
-    resultElement.innerHTML = "Ответ: Здравствуйте, " + inputNameElement.value + " " + inputSurnameElement.value + "!"
-});
